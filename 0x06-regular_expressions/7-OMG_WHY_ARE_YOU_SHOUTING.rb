a0accc240420d63e97b0ef9c6c32eaa78a0cd563
edwardzuniga <4593@holbertonschool.com>
2022-06-28 16:06:29 -0500

    cmmp

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
-# the regular expression must be
-# only matching: capital letters
-puts ARGV[0].scan(/[A-Z]/.join
+# the regular expression must be only matching: capital letters
+puts ARGV[0].scan(/[A-Z]/).join
